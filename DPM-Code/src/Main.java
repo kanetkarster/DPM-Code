90e933ec9470568e9cf268942baafafad5162bb5
kanetkarster <kanetkarster@gmail.com>
2014-04-03 11:51:21 -0400

    made SquareRunner work properly

--- DPM-Code/src/Main.java
+++ DPM-Code/src/Main.java
@@ -18,10 +18,32 @@ public class Main {
 	public static Odometer odo;
 	public static boolean hasBlock = false;
 	public static void main(String[] args) {
-		driver.travel(0, 60);
-		driver.travel(60, 60);
-		driver.travel(60, 0);
-		driver.travel(0, 0);
+		
+		//sets xDest, yDest and block ID
+		//getBluetooth();
+		//after Bluetooth input received:
+		ColorSensor cs = new ColorSensor(SensorPort.S1);
+		UltrasonicSensor us1 = new UltrasonicSensor(SensorPort.S2);
+		UltrasonicSensor us2 = new UltrasonicSensor(SensorPort.S4);
+		ColorSensor blockSensor = new ColorSensor(SensorPort.S3);
+
+		UltrasonicPoller usPoller = new UltrasonicPoller(us1);
+		UltrasonicPoller usPoller2 = new UltrasonicPoller(us2);
+		odo = new Odometer();
+		driver = new Driver(odo);
+		blockDetector = new BlockDetection(usPoller, usPoller2, blockSensor, blockID);
+		//OdometryDisplay lcd = new OdometryDisplay(odo, blockDetector, usPoller);
+		OdometryDisplay lcd = new OdometryDisplay(odo, blockDetector, usPoller2);
+
+		lcd.start();
+		odo.start();
+		
+		while(Button.waitForAnyPress() == 0);
+		
+		driver.travel(0, 60, false);
+		driver.travel(60, 60, false);
+		driver.travel(60, 0, false);
+		driver.travel(0, 0, false);
 	}
 	/**
 	 * Block avoidance method:
