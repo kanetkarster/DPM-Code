7597cc7d07baa82156f929996e62d98be040b09e
kanetkarster <kanetkarster@gmail.com>
2014-04-06 13:04:35 -0400

    Increased localization speed

--- DPM-Code/src/LightLocalizer.java
+++ DPM-Code/src/LightLocalizer.java
@@ -39,7 +39,7 @@ public class LightLocalizer {
 	cs.setFloodlight(lejos.robotics.Color.RED);
 	cs.calibrateHigh();
 	double lv = cs.getNormalizedLightValue() - 110;
-	robot.rotate(true, 100);
+	robot.rotate(true, 125);
 	while (odo.getTheta() * 180 / Math.PI <= 358){
 		try {Thread.sleep(50);} catch (InterruptedException e) {}
 		lightValue = cs.getNormalizedLightValue();
